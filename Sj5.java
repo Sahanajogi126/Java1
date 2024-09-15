11029c7ef6eff6346277859e7811e83dbc787438
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2024-09-15 12:11:22 +0530

    Sj5.java

---
+++ Sj5.java
@@ -0,0 +1,12 @@
+public class Sj5{
+    public static void main(String[] args){
+        int num1=Integer.parseInt(args[0]);
+        int factn=1;
+	for (int i=1;i<num1+1;i++){
+	    factn=factn*i;
+	}
+	System.out.println(factn);
+	
+    }
+}
+
