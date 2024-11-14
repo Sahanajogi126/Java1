2a8bbd89f0410131de8167b3066a4871bc3b923b
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2024-11-14 10:04:31 +0530

    Add files via upload

---
+++ TestTools.java
@@ -0,0 +1,19 @@
+package pkg1;
+    public class TestTools{
+        public static void main(String[] args){
+             Tools t1=new Tools();
+             System.out.println("numbers(10):");
+             t1.numbers(10);
+             System.out.println("even(20):");
+             t1.even(20);
+             System.out.println("odd(19):");
+             t1.odd(19);
+             System.out.println("mul3:");
+             t1.mul3();
+             System.out.println("mul4:");
+             t1.mul4();
+             System.out.println("mul5:");
+             t1.mul5();
+             
+    }
+}
---
+++ Tools.java
@@ -0,0 +1,52 @@
+package pkg1;
+public class Tools {
+    public void numbers(int n1) {
+        for(int i=0;i<=n1;i++)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void even(int n){
+        for(int i=0;i<=n;i+=2){
+            System.out.print(i+" "); 
+        }
+        System.out.println();
+    }
+    public void odd(int n){
+        for(int i=1;i<=n;i+=2){
+            System.out.print(i+" "); 
+            
+        }
+        System.out.println();    
+    }
+    public void mul3(){
+        for(int i=0;i<=30;i+=3){
+            System.out.print(i+" ");        
+        }
+        System.out.println();       
+    }
+    public void mul4(){
+        for(int i=0;i<=32;i+=4){
+            System.out.print(i+" ");        
+        }
+        System.out.println();       
+    }
+    public void mul5(){
+        for(int i=0;i<=30;i+=5){
+            System.out.print(i+" ");        
+        }
+        System.out.println();       
+    }
+}
+
+        
+    
+    
+    
+        
+
+        
+    
+     
+
