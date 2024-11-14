a7b13a0ce7a279b6ee9f873c9577936f6efb5629
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2024-11-14 10:44:55 +0530

    Tools.java

--- Tools.java
+++ Tools.java
@@ -8,33 +8,33 @@ public class Tools {
         System.out.println();
     }
     public void even(int n){
-        for(int i=0;i<=n;i+=2){
-            System.out.print(i+" "); 
+        for(int i=0;i<=n;i++){
+            System.out.print(i*2+" "); 
         }
         System.out.println();
     }
     public void odd(int n){
-        for(int i=1;i<=n;i+=2){
-            System.out.print(i+" "); 
+        for(int i=1;i<=n;i++){
+            System.out.print(2*i+1+" "); 
             
         }
         System.out.println();    
     }
     public void mul3(int n){
-        for(int i=0;i<=n;i+=3){
-            System.out.print(i+" ");        
+        for(int i=0;i<=n;i++){
+            System.out.print(i*3+" ");        
         }
         System.out.println();       
     }
     public void mul4(int n){
-        for(int i=0;i<=n;i+=4){
-            System.out.print(i+" ");        
+        for(int i=0;i<=n;i++){
+            System.out.print(i*4+" ");        
         }
         System.out.println();       
     }
     public void mul5(int n){
-        for(int i=0;i<=n;i+=5){
-            System.out.print(i+" ");        
+        for(int i=0;i<=n;i++){
+            System.out.print(i*5+" ");        
         }
         System.out.println();       
     }
