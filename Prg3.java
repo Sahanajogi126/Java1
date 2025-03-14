ff836596a7827d04338bfa240bb813652342d310
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 10:22:31 +0530

    Prg3.java

---
+++ Prg3.java
@@ -0,0 +1,22 @@
+public class Prg3 {
+    public static void main(String[] args){
+        int num=10;
+        if(num%2==0&&num%3==0)
+        {
+            System.out.println("java");
+        }
+        else if(num%2==0)
+        {
+           System.out.println("python"); 
+        }
+        else if(num%3==0)
+        {
+           System.out.println("c/c++"); 
+        }
+        else
+        {
+           System.out.println(".net");  
+        }
+    }
+    
+}
