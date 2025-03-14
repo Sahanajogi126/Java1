607c16dc6820d417b863e77fc51f5d79aa42b824
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 10:29:25 +0530

    Prg4.java

---
+++ Prg4.java
@@ -0,0 +1,18 @@
+public class Prg4 {
+    public static void main(String[] args){
+        int age=66;
+        if(age<=12)
+        {
+            System.out.println("Minor ticket");
+        }
+        else if(age>12&&age<=60)
+        {
+            System.out.println("Normal ticket");
+        }
+        else
+        {
+            System.out.println("Senior Citizen ticket");
+        }
+    }
+    
+}
