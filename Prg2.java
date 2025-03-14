24d25cb3605cf3a6eaf5be377a2c4fd4e8b623c6
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 10:09:13 +0530

    Prg2.java

---
+++ Prg2.java
@@ -0,0 +1,16 @@
+public class Prg2 {
+    public static void main(String[] args){
+        int age=12;
+        if(age>18)
+        {
+            System.out.println("Major:ETV");
+            
+        }
+        else
+        {
+            System.out.println("Minor:NETV");
+        }
+        
+    }
+    
+}
