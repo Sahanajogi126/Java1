7f26dc9818ec2ae0d8f145c6215fcaba4072e69d
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 09:56:10 +0530

    prg8.java

---
+++ prg8.java
@@ -0,0 +1,20 @@
+public class prg8 {
+    public static void main(String[] args){
+        int x=7;
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x);
+        
+        int i=8;
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(--i);
+        System.out.println(--i);
+        System.out.println(i++);
+        System.out.println(i);
+        
+    }
+    
+}
