88d3ee4dc4a192c1a371098c57b3db922385c7c2
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 09:48:53 +0530

    Java4.java

---
+++ Java4.java
@@ -0,0 +1,25 @@
+public class Java4 {
+    public static void main(String[] args){
+        int val1=10;
+        double val2=2.5;
+        char val3='s';
+        boolean val4=true;
+        long val5=12345678l;
+        float val7=123.55f;
+        String val6="sahana";
+        int m1=30;
+        m1=60;
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val3);
+        System.out.println(val4);
+        System.out.println(val5);
+        System.out.println(val6);
+        System.out.println(val7);
+        System.out.println(val5+" "+val7);
+        System.out.println(m1);
+                
+        
+    }
+    
+}
