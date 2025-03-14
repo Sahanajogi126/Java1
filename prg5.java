dfea82f1149473f3043d6393fcdbb2f5fa8255ea
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 09:45:47 +0530

    prg5.java

---
+++ prg5.java
@@ -0,0 +1,16 @@
+public class prg5 {
+    public static void main(String[] args){
+        
+        System.out.println(10+5-2);
+        System.out.println(10-5+5);
+        System.out.println(10*2/2);
+        System.out.println(10/2+5*2);
+        System.out.println(10+2*5+3);
+        System.out.println(4*3+7*2);
+        System.out.println(15/3*4);
+        System.out.println(10/2*6/3);
+        
+        
+    }
+    
+}
