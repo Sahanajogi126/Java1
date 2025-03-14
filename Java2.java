77a5af825b27f057b4ea4eeed22e764648ba13e8
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-14 09:46:32 +0530

    Java2.java

---
+++ Java2.java
@@ -0,0 +1,12 @@
+public class Java2 {
+    public static void main(String[] args)
+    {
+        System.out.println("Sahana"+"Jogi");
+        System.out.println('J'+10);
+        System.out.println(2025+20+"boys");
+        System.out.println("girls"+20+20);
+        System.out.println(true);
+        System.out.println(false);
+    }
+    
+}
