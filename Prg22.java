ea1f4ee081ff04e4b340c65f4154d6fc64f4e148
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 12:46:15 +0530

    Prg22.java

---
+++ Prg22.java
@@ -0,0 +1,13 @@
+public class Prg22 {
+    public static void main(String[] args){
+        int x=0;
+        do
+        {
+            System.out.println("Pune");
+            x++;
+        }
+        while(x<5);
+      
+    }
+    
+}
