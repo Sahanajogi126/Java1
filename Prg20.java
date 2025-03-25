034fb9265282de96508b72f328ab9da7b2729d2b
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 12:39:02 +0530

    Prg20.java

---
+++ Prg20.java
@@ -0,0 +1,13 @@
+public class Prg20 {
+    public static void main(String[] args){
+        System.out.println("Program starts");
+        int n=5;
+        while(n>0)
+        {
+            System.out.println("AI Developer");
+            n--;
+        }
+        System.out.println("Program ends");
+    }
+    
+}
