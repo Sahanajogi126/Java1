6c24a3bfb7bc80e0db1eb1ad9f439fb7e6bb73eb
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 11:41:38 +0530

    Prg12.java

---
+++ Prg12.java
@@ -0,0 +1,13 @@
+public class Prg12 {
+  public static void main(String[] args) {
+        int n=5;
+        char ch='a';
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(ch+" ");
+            ch++;
+        }
+        
+    }
+    
+}
