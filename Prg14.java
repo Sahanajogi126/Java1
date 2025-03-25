c25d889c86332e768777ccc3c4d19809c733eb96
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 11:50:17 +0530

    Prg14.java

---
+++ Prg14.java
@@ -0,0 +1,16 @@
+public class Prg14 {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print("X"+" ");
+            }
+            else
+            {
+                System.out.print("O"+" ");
+            }
+        }
+    }
+    
+}
