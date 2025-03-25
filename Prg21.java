faaa0bf44e0425a33334113ed33f87de4737d1f1
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 12:43:16 +0530

    Prg21.java

---
+++ Prg21.java
@@ -0,0 +1,13 @@
+public class Prg21 {
+    public static void main(String[] args){
+        System.out.println("Program starts");
+        int n=10;
+        while(n>0)
+        {
+            System.out.println(n);
+            n=n-3;
+        }
+        
+    }
+    
+}
