416c26dab651cbfcc016b8c367fc72657629c961
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 11:34:43 +0530

    Prg11.java

---
+++ Prg11.java
@@ -0,0 +1,9 @@
+public class Prg11 {
+    public static void main(String[] args){
+        for(int x=0;x<10;x=x+2)
+        {
+            System.out.println("Senior Test Engineer");
+        }
+    }
+    
+}
