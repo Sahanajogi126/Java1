b82afe90d0c65bf667b5ce5e6090035f8fd827ed
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-25 12:49:49 +0530

    Prg23.java

---
+++ Prg23.java
@@ -0,0 +1,13 @@
+public class Prg23 {
+    public static void main(String[] args){
+        int n=7;
+        do
+        {
+            System.out.println("Java");
+            n--;
+        }
+        while(n<5);
+      
+    }
+    
+}
