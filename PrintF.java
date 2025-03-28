050365ae1d461dae91e951925603ebd9ace03df8
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 08:48:37 +0530

    PrintF.java

---
+++ PrintF.java
@@ -0,0 +1,18 @@
+public class PrintF {
+    public static void main(String[] args){
+        int n = 5; // Size of the letter
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == n / 2) {
+                    System.out.print("* "); // Top and middle horizontal lines
+                } else if (j == 0) {
+                    System.out.print("* "); // Left vertical line
+                } else {
+                    System.out.print("  "); // Space
+                }
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
