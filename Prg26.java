084a7ba3b57ee922f9eb1a478296940ada11460d
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 09:53:21 +0530

    Prg26.java

---
+++ Prg26.java
@@ -0,0 +1,17 @@
+public class Prg26 {
+    static int disp()
+    {
+        return 45;
+    }
+    static char print()
+    {
+        return 'a';
+    }
+    public static void main(String[] args){
+        int res=disp();
+        System.out.println(res);
+        System.out.println(print());
+        
+    }
+    
+}
