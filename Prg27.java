87b0aca84857aec1584ebc8999339569260d7c26
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 09:59:46 +0530

    Prg27.java

---
+++ Prg27.java
@@ -0,0 +1,17 @@
+public class Prg27 {
+    static int sq(int a)
+    {
+        return a*a;
+    }
+    static int cb(int b)
+    {
+        int ans=b*b*b;
+        return ans;
+    }
+    public static void main(String[] args){
+        System.out.println(sq(2));
+        System.out.println(cb(3));
+        
+    }
+    
+}
