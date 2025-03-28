67ced0fe2c498ea9ca1fbae3352901cec2116ac1
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 09:37:20 +0530

    Prg24.java

---
+++ Prg24.java
@@ -0,0 +1,13 @@
+public class Prg24{
+    static void greetings()
+    {
+        System.out.println("happy morning");
+    }
+    public static void main(String[] args){
+        System.out.println("program starts");
+        greetings();
+        System.out.println("program ends");
+        
+    }
+    
+}
