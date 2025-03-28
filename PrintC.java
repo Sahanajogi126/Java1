cf6db4459a9b5b86925fd11d71ff0279d03a7a6f
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 08:46:58 +0530

    PrintC.java

---
+++ PrintC.java
@@ -0,0 +1,18 @@
+public class PrintC {
+    public static void main(String[] args){
+        int n = 5; // Size of the letter
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || i == n - 1) {
+                    System.out.print("* "); // Top and bottom horizontal lines
+                } else if (j == 0) {
+                    System.out.print("* "); // Left vertical line
+                } else {
+                    System.out.print("  "); // Space
+                }
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
