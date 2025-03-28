b74cf24892b37aa0b180cdc95fb60a09f7512a36
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-28 09:45:19 +0530

    Prg25.java

---
+++ Prg25.java
@@ -0,0 +1,15 @@
+public class Prg25 {
+    static void add(int a, int b)
+    {
+        System.out.println("sum of a+b:"+(a+b));
+    }
+    public static void main(String[] args){
+         System.out.println("program starts");
+         add(10,2);
+         add(4,6);
+         
+          System.out.println("program ends");
+        
+    }
+    
+}
