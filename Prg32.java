0f95f3a653c677703d97585bcaedead61e61c4da
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-04 09:44:04 +0530

    Prg32.java

---
+++ Prg32.java
@@ -0,0 +1,11 @@
+public class Prg32 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
