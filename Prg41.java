018ab57aeaed2ac67649de0afce4cc6ff28a0ea3
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-08 12:44:06 +0530

    Prg41.java

---
+++ Prg41.java
@@ -0,0 +1,20 @@
+public class Prg41 {
+    public static void main(String[] args){
+        int[] arr={21,22,23,24,25};
+        int evencount=0;
+        int oddcount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                evencount++;
+            }
+            else{
+                oddcount++;
+            }
+        }
+        System.out.println("Even count: "+evencount);
+        System.out.println("Odd count: "+oddcount);
+        
+                
+    }
+    
+}
