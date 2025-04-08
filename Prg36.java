bca15122f4c9d0d7cb12b29db34c05d7b2c5ec2f
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-08 12:01:12 +0530

    Prg36.java

---
+++ Prg36.java
@@ -0,0 +1,10 @@
+public class Prg36 {
+    public static void main(String[] args){
+        int[] arr={11,12,13,14,15};
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
