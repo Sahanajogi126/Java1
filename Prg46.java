2f03b14afd9da08bde96defda0f5e7fad82a3c45
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-11 10:03:28 +0530

    Prg46.java

---
+++ Prg46.java
@@ -0,0 +1,13 @@
+public class Prg46 {
+    public static void main(String[] args){
+        int a=25;
+        int sq=a*a;
+        if(sq%10==a){
+            System.out.println("Automorphic value");
+        }
+        else{
+            System.out.println("Not automorphic value");
+        }
+    }
+    
+}
