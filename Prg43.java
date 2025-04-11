41da9ffe2344a492b9944aa1e41515f493c5ae57
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-11 09:39:07 +0530

    Prg43.java

---
+++ Prg43.java
@@ -0,0 +1,12 @@
+public class Prg43 {
+    public static void main(String[] args){
+        int n=12345;
+        int rem=0;
+        while(n>0){
+            rem=n%10;
+            System.out.print(rem);
+            n=n/10;
+        }
+    }
+    
+}
