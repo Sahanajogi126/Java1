8a323f47258a17f53edfdd50f86fe668db6e8b21
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-11 10:15:25 +0530

    Prg47.java

---
+++ Prg47.java
@@ -0,0 +1,17 @@
+public class Prg47 {
+    public static void main(String[] args){
+        int n=1010;
+        int r=0;
+        int base=2;
+        int i=0;
+        int sum=0;
+        while(n>0){
+            r=n%10;
+            sum=(int) (sum+r*Math.pow(base,i));
+            i++;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+    
+}
