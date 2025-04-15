1787452b3ac1e61ce6053ad3d9ec468f763fe6cb
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-15 12:12:43 +0530

    Prg48.java

---
+++ Prg48.java
@@ -0,0 +1,13 @@
+class Student{
+    int age=10;
+    String name="Tom";
+}
+public class Prg48 {
+    public static void main(String[] args){
+        Student S= new Student();
+        System.out.println(S.age);
+        System.out.println(S.name);
+        
+    }
+    
+}
