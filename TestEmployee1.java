5027cef11e9928f82a894f8ab85c22c8f16efa04
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-15 12:44:18 +0530

    TestEmployee1.java

---
+++ TestEmployee1.java
@@ -0,0 +1,10 @@
+package javaapplication257;
+
+
+public class TestEmployee1 {
+    public static void main(String[] args){
+        System.out.println(Employee1.id);
+        System.out.println(Employee1.des);
+        Employee1.work();
+    }
+}
