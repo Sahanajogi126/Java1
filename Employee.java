771b51d0bb953b80c7a25b2203b8f5b1396eda2d
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-15 12:36:05 +0530

    Employee.java

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    int id=101;
+    String des="Manager";
+    double sal=12345.67;
+    void work(){
+        System.out.println("Working");
+    }
+    public static void main(String[] args){
+        
+    }
+    
+}
