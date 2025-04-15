75420f0646790dd00113d68651978e321c8759cf
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-15 12:36:54 +0530

    TestEmployee.java

---
+++ TestEmployee.java
@@ -0,0 +1,9 @@
+public class TestEmployee {
+    public static void main(String[] args){
+        Employee e= new Employee();
+        System.out.println(e.id);
+        System.out.println(e.des);
+        System.out.println(e.sal);
+        e.work();
+    }
+}
