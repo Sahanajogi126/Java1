d29dc131923734b7049d046bf2d19eb16479704f
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-15 12:43:10 +0530

    Employee1.java

---
+++ Employee1.java
@@ -0,0 +1,14 @@
+package javaapplication257;
+
+
+public class Employee1 {
+    static int id=101;
+    static String des="Manager";
+    static void work(){
+        System.out.println("Working");
+    }
+    public static void main(String[] args){
+        
+    }
+    
+}
