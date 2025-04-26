41ef5d2f37f62c9c289221ef09dd1e140ef3c3d2
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-04-26 09:28:07 +0530

    SecondMinimum.java

---
+++ SecondMinimum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SecondMinimum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            if (num < min) {
+                secondMin = min;
+                min = num;
+            } else if (num < secondMin && num != min) {
+                secondMin = num;
+            }
+        }
+
+        if (secondMin == Integer.MAX_VALUE) {
+            System.out.println("No second minimum element");
+        } else {
+            System.out.println("Second minimum is: " + secondMin);
+        }
+    }
+}
