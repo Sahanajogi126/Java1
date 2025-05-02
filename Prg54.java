4868a1500d832ddba76f6071125cb8f80e589bb1
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-05-02 10:14:26 +0530

    Prg54.java

---
+++ Prg54.java
@@ -0,0 +1,11 @@
+public class Prg54 {
+    public static void main(String[] args){
+        String s1="java developer";
+        String s2="Java Tester";
+        System.out.println(s1.substring(2));
+        System.out.println(s1.substring(0,6));
+        System.out.println(s2.trim());
+        
+    }
+    
+}
