39c0ea70f1b5f9d1e2aea0531df357b73f7b0485
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-05-02 10:15:02 +0530

    Prg55.java

---
+++ Prg55.java
@@ -0,0 +1,10 @@
+public class Prg55 {
+    public static void main(String[] args){
+        String cars="kia,bmw,benz";
+        String[] s=cars.split(",");
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
+    }
+    
+}
