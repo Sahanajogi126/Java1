5e5d0884e9792924a9dc75828708628cd41d11c8
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-05-02 10:07:40 +0530

    Prg51.java

---
+++ Prg51.java
@@ -0,0 +1,10 @@
+public class Prg51 {
+    public static void main(String[] args){
+        String s="Java Developer";
+        System.out.println(s.charAt(3));
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+    
+}
