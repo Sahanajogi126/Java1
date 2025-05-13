9ab802f5d41dc85d2d3ec50612447da6a6003b84
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-05-13 12:51:21 +0530

    BinarySearch1.java

---
+++ BinarySearch1.java
@@ -0,0 +1,43 @@
+public class BinarySearch1 {
+    int binarysearch(int[] arr,int l,int r,int ele)
+    {
+        while(l<=r)
+        {
+           int mid=(l+r)/2;
+           if(arr[mid]==ele)
+           {
+               return mid;
+           }
+           else if(arr[mid]<ele)
+           {
+              l=mid+1; 
+           }
+           else if(arr[mid]>ele)
+           {
+               r=mid-1;
+           }
+        }
+        return -1;
+      
+        
+    }
+    public static void main(String[] args)
+    {
+        int[] arr={5,9,17,23,25,45,59,63,71,89};
+        int l=0;
+        int r=arr.length-1;
+        int ele=58;
+        BinarySearch1 b=new BinarySearch1();
+        int res=b.binarysearch(arr,l,r,ele);
+        if(res!=-1)
+        {
+            System.out.println("element found");
+            
+        }
+        else
+        {
+            System.out.println("element not found");
+        }
+    }
+    
+}
