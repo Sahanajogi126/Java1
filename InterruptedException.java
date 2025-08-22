e5e1a9f5c774d0d9f6d4a2b5cdb9c5a1a4299ea8
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-08-22 10:28:55 +0530

    InterruptedException.java

---
+++ InterruptedException.java
@@ -0,0 +1,16 @@
+package com;
+
+public class InterruptedException {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(2000);
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
